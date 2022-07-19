61dc734e75ecf11f960340dcab0f051266b302f4
pabloberry44 <pawel.borowiec@tutanota.com>
2022-07-19 09:23:40 +0200

    create game grid and buttons

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <nav>
+    <p>Tic - Tac - Toe</p>
+  </nav>
+
+  <main>
+    <div class="gameGrid">
+      <input 
+        type="button" 
+        value=""
+        v-for="button in buttons">
+    </div>
+  </main>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+  import { ref } from 'vue'
+ 
+  const buttons = ref([])
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  for (let i = 0; i < 9; i++) {
+
+    buttons.value.push(
+      {
+        id: i,
+        value: ''
+      }
+    )
+
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  html * {
+    font-family: Arial, Helvetica, sans-serif;
+    box-sizing: border-box;
+    margin: 0%;
+    padding: 0%;
+  }
+
+  nav {
+    background: lightcyan;
+    padding: .3em;
+    font-size: 2rem;
+    text-align: center;
+  }
+
+  main {
+    width: 100%;
+    height: 80vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .gameGrid {
+    width: 330px;
+    aspect-ratio: 1/1;
+    background: lightcoral;
+  }
+
+    .gameGrid input {
+      width: 100px;
+      aspect-ratio: 1/1;
+      margin: 5px;
+    }
+
 </style>
