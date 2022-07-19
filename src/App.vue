cf66865bfd26d6657a2b1061b98c97bbc180e0e2
pabloberry44 <pawel.borowiec@tutanota.com>
2022-07-19 11:12:35 +0200

    winning conditions and win view added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
 
-  <nav>
-    <p>Tic - Tac - Toe</p>
-  </nav>
-
   <main>
 
     <p class="nextTurn">
       <span :="nextTurn.value">{{nextTurn}}</span> turn
     </p>
+    
 
     <div class="gameGrid">
       <button        
@@ -18,7 +15,22 @@
         {{button.text}}
       </button>
 
+      <div class="winView" v-if="winner" :="winner.value">
+        <div class="normal">
+          <p>The winner is {{winner}}!</p>
+        </div>
+        <button @click="resetGrid"><ion-icon name="refresh-outline"></ion-icon></button>
+        <div class="flipped">
+          <p>The winner is {{winner}}!</p>
+        </div>
+
+      </div>
+
     </div>
+
+    <p class="nextTurn flipped">
+      <span :="nextTurn.value">{{nextTurn}}</span> turn
+    </p>
 
   </main>
 
@@ -45,11 +57,49 @@
 
     if( buttons.value[buttonId].text == '' ) {
       buttons.value[buttonId].text = nextTurn.value
+      nextTurn.value = nextTurn.value == 'O' ? 'X' : 'O'
     }
+    checkWin()
 
-    nextTurn.value = nextTurn.value == 'O' ? 'X' : 'O'
   }
 
+  const winner = ref('')
+  const checkWin = () => {
+
+    if(
+      (buttons.value[0].text + buttons.value[1].text + buttons.value[2].text == 'OOO') || 
+      (buttons.value[3].text + buttons.value[4].text + buttons.value[5].text == 'OOO') ||
+      (buttons.value[6].text + buttons.value[7].text + buttons.value[8].text == 'OOO') ||
+      (buttons.value[0].text + buttons.value[3].text + buttons.value[6].text == 'OOO') ||
+      (buttons.value[1].text + buttons.value[4].text + buttons.value[7].text == 'OOO') ||
+      (buttons.value[2].text + buttons.value[5].text + buttons.value[8].text == 'OOO') ||
+      (buttons.value[0].text + buttons.value[4].text + buttons.value[8].text == 'OOO') ||
+      (buttons.value[2].text + buttons.value[4].text + buttons.value[6].text == 'OOO')
+      ){
+        winner.value = 'O'
+      }
+
+    if(
+      (buttons.value[0].text + buttons.value[1].text + buttons.value[2].text == 'XXX') || 
+      (buttons.value[3].text + buttons.value[4].text + buttons.value[5].text == 'XXX') ||
+      (buttons.value[6].text + buttons.value[7].text + buttons.value[8].text == 'XXX') ||
+      (buttons.value[0].text + buttons.value[3].text + buttons.value[6].text == 'XXX') ||
+      (buttons.value[1].text + buttons.value[4].text + buttons.value[7].text == 'XXX') ||
+      (buttons.value[2].text + buttons.value[5].text + buttons.value[8].text == 'XXX') ||
+      (buttons.value[0].text + buttons.value[4].text + buttons.value[8].text == 'XXX') ||
+      (buttons.value[2].text + buttons.value[4].text + buttons.value[6].text == 'XXX')
+      ){
+        winner.value = 'X'
+      }
+
+  }
+
+  const resetGrid = () => {
+    for (let i = 0; i < 9; i++) {
+      buttons.value[i].text = ''
+    }
+    winner.value = ''
+  }
 </script>
 
 <style>
@@ -61,16 +111,9 @@
     padding: 0%;
   }
 
-  nav {
-    background: lightcyan;
-    padding: .3em;
-    font-size: 2rem;
-    text-align: center;
-  }
-
   main {
     width: 100%;
-    height: 80vh;
+    height: 90vh;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -85,17 +128,24 @@
     }
 
   .gameGrid {
+    position: relative;
     width: 340px;
     aspect-ratio: 1/1;
-    background: lightcoral;
+    background: #BCD9BA;
     display: flex;
     flex-wrap: wrap;
     padding: 10px;
     gap: 10px;
     border-radius: 20px;
+    overflow: hidden;
+    box-shadow:
+      0px 0px 5.3px rgba(0, 0, 0, 0.028),
+      0px 0px 17.9px rgba(0, 0, 0, 0.042),
+      0px 0px 80px rgba(0, 0, 0, 0.07);
   }
 
     .gameGrid button {
+      background: #C5E3C3;
       cursor: pointer;
       width: 100px;
       aspect-ratio: 1/1;
@@ -105,7 +155,47 @@
       font-size: 2rem;
       border-radius: 20px;
       border: 1px solid gray;
+      color: black;
       box-shadow: 0px 0px 5.3px rgba(0, 0, 0, 0.028);
     }
+
+  .winView {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 340px;
+    aspect-ratio: 1/1;
+    background: #c5dac4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 3em;
+  }
+    .winView div {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+    .winView p {
+      font-size: 2rem;
+    }
+
+    .winView button {
+      width: max-content;
+      border-radius: 50%;
+      padding: .3em;
+      background: #abbbaa;
+      border: none;
+      box-shadow:
+        0px 0px 5.3px rgba(0, 0, 0, 0.028),
+        0px 0px 17.9px rgba(0, 0, 0, 0.042),
+        0px 0px 80px rgba(0, 0, 0, 0.07);
+    }
+
+  .flipped {
+    transform: rotateZ(180deg);
+  }
 
 </style>
